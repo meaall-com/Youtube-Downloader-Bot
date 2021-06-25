020159684f9fb31e5d0192d991e9d5dc5fdb9143
meaall-com <67676419+meaall-com@users.noreply.github.com>
2021-06-25 19:49:27 +0300

    Create youtube_callback_data.py

---
+++ plugins/youtube_callback_data.py
@@ -0,0 +1,296 @@
+import asyncio
+
+import os
+
+from pyrogram import (Client,
+
+                      Filters,
+
+                      InlineKeyboardButton,
+
+                      InlineKeyboardMarkup,
+
+                      ContinuePropagation,
+
+                      InputMediaDocument,
+
+                      InputMediaVideo,
+
+                      InputMediaAudio)
+
+from helper.ffmfunc import duration
+
+from helper.ytdlfunc import downloadvideocli, downloadaudiocli, extractYt, create_buttons
+
+from PIL import Image
+
+from hachoir.metadata import extractMetadata
+
+from hachoir.parser import createParser
+
+from datetime import datetime, timedelta
+
+from bot import user_time
+
+from config import youtube_next_fetch
+
+import wget
+
+import os
+
+from PIL import Image
+
+ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
+
+@Client.on_message(Filters.regex(ytregex))
+
+async def ytdl(_, message):
+
+    userLastDownloadTime = user_time.get(message.chat.id)
+
+    try:
+
+        if userLastDownloadTime > datetime.now():
+
+            wait_time = round((userLastDownloadTime - datetime.now()).total_seconds() / 60, 2)
+
+            await message.reply_text(f"`Wait {wait_time} Minutes before next Request`")
+
+            return
+
+    except:
+
+        pass
+
+    url = message.text.strip()
+
+    await message.reply_chat_action("typing")
+
+    try:
+
+        title, thumbnail_url, formats = extractYt(url)
+
+        now = datetime.now()
+
+        user_time[message.chat.id] = now + \
+
+                                     timedelta(minutes=youtube_next_fetch)
+
+    except Exception:
+
+        await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
+
+        return
+
+    buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
+
+    sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
+
+    try:
+
+        # Todo add webp image support in thumbnail by default not supported by pyrogram
+
+        # https://www.youtube.com/watch?v=lTTajzrSkCw
+
+        img = wget.download(thumbnail_url)
+
+        im = Image.open(img).convert("RGB")
+
+        output_directory = os.path.join(os.getcwd(), "downloads", str(message.chat.id))
+
+        if not os.path.isdir(output_directory):
+
+            os.makedirs(output_directory)
+
+        thumb_image_path = f"{output_directory}.jpg"
+
+        im.save(thumb_image_path,"jpeg")
+
+        await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
+
+        await sentm.delete()
+
+    except Exception as e:
+
+        print(e)
+
+        try:
+
+            thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
+
+            await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
+
+        except Exception as e:
+
+            await sentm.edit(
+
+            f"<code>{e}</code> #Error")
+
+async def catch_youtube_fmtid(c, m):
+
+    cb_data = m.data
+
+    if url:
+
+        yturl = url
+
+        format_id = int(243)
+
+        media_type = 'video'
+
+        print(media_type)
+
+    else:
+
+        raise ContinuePropagation
+
+async def catch_youtube_dldata(c, q):
+
+    cb_data = q.data.strip()
+
+    #print(q.message.chat.id)
+
+    # Callback Data Check
+
+    yturl = url
+
+    format_id = int(243)
+
+    thumb_image_path = "/app/downloads" + \
+
+        "/" + str(q.message.chat.id) + ".jpg"
+
+    print(thumb_image_path)
+
+    if os.path.exists(thumb_image_path):
+
+        width = 0
+
+        height = 0
+
+        metadata = extractMetadata(createParser(thumb_image_path))
+
+        #print(metadata)
+
+        if metadata.has("width"):
+
+            width = metadata.get("width")
+
+        if metadata.has("height"):
+
+            height = metadata.get("height")
+
+        img = Image.open(thumb_image_path)
+
+        if cb_data.startswith(("audio", "docaudio", "docvideo")):
+
+            img.resize((320, height))
+
+        else:
+
+            img.resize((90, height))
+
+        img.save(thumb_image_path, "JPEG")
+
+     #   print(thumb_image
+
+    filext = "%(title)s.%(ext)s"
+
+    userdir = os.path.join(os.getcwd(), "downloads", str(q.message.chat.id))
+
+    if not os.path.isdir(userdir):
+
+        os.makedirs(userdir)
+
+    await q.edit_message_reply_markup(
+
+        InlineKeyboardMarkup([[InlineKeyboardButton("Downloading...", callback_data="down")]]))
+
+    filepath = os.path.join(userdir, filext)
+
+    # await q.edit_message_reply_markup([[InlineKeyboardButton("Processing..")]])
+
+    video_command = [
+
+        "youtube-dl",
+
+        "-c",
+
+        "--embed-subs",
+
+        "-f", f"{format_id}+bestaudio",
+
+        "-o", filepath,
+
+        "--hls-prefer-ffmpeg", yturl]
+
+    loop = asyncio.get_event_loop()
+
+    med = None
+
+    if url:
+
+        filename = await downloadvideocli(video_command)
+
+        dur = round(duration(filename))
+
+        med = InputMediaVideo(
+
+            media=filename,
+
+            duration=dur,
+
+            width=width,
+
+            height=height,
+
+            thumb=thumb_image_path,
+
+            caption=os.path.basename(filename),
+
+            supports_streaming=True
+
+        )
+
+    if med:
+
+        loop.create_task(send_file(c, q, med, filename))
+
+    else:
+
+        print("med not found")
+
+async def send_file(c, q, med, filename):
+
+    print(med)
+
+    try:
+
+        await q.edit_message_reply_markup(
+
+            InlineKeyboardMarkup([[InlineKeyboardButton("Uploading...", callback_data="down")]]))
+
+        await c.send_chat_action(chat_id=q.message.chat.id, action="upload_document")
+
+        # this one is not working
+
+        await q.edit_message_media(media=med)
+
+    except Exception as e:
+
+        print(e)
+
+        await q.edit_message_text(e)
+
+    finally:
+
+        try:
+
+            os.remove(filename)
+
+            os.remove(thumb_image_path)
+
+        except:
+
+            pass
+
