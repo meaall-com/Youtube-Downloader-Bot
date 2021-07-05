26ef6f65f52b2f0cbb1ea5f5189a62951595010c
meaall-com <67676419+meaall-com@users.noreply.github.com>
2021-07-05 22:07:47 +0300

    Update youtube_callback_data.py

--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -116,7 +116,7 @@ async def ytdl(_, message):
 
     await sentm.reply_chat_action("upload_video")
         # this one is not working
-    await sentm.edit_media(med)
+    await sentm.reply_video(med)
 
 
 
